2196dc08fae5354045fda61f27e11f5ce3f026b5
AJ Montgomery <aj.monty@gmail.com>
2026-01-13 15:10:56 +0000

    Added roulette wheel to welcome screen with infinite rotating animation

--- src/app/pages/welcome/welcome.page.ts
+++ src/app/pages/welcome/welcome.page.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { RouletteWheelComponent } from '../../core/components/roulette-wheel/roulette-wheel.component';
+import { GameStateService } from '../../core/services/game-state.service';
 
 @Component({
   selector: 'app-welcome.page',
-  imports: [RouterLink],
+  imports: [RouterLink, RouletteWheelComponent],
   templateUrl: './welcome.page.html',
   styleUrl: './welcome.page.scss',
 })
-export class WelcomePage {}
+export class WelcomePage {
+  state = inject(GameStateService);
+}
